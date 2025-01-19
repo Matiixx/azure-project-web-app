ab83c8f1059fa179416504c8d094d8568b163066
Matiixx <87618425+Matiixx@users.noreply.github.com>
2025-01-19 14:38:43 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    "process.env": process.env,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    define: {
+      "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+    },
+  };
 });
